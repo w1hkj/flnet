e95afd50d1e6fbd8e7acd18a5312b0724e28b949
Robert Cunnings <nw8l@whitemesa.com>
2016-07-04 08:23:05 -0500

    mxe update
      * corrected mxe build scripts

--- scripts/buildmxe.sh
+++ scripts/buildmxe.sh
@@ -1,13 +1,11 @@
-# build file to generate the distribution binary tarball
-
-make clean
-autoreconf
+#!/bin/sh
 
 ./configure \
   $PKGCFG \
   $CROSSCFG \
   --with-ptw32=$PREFIX/i686-w64-mingw32.static \
-  PTW32_LIBS="-lpthread -lpcreposix -lpcre" \
+  --enable-static \
+  PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config
 
 make
@@ -15,10 +13,3 @@ make
 $PREFIX/bin/i686-w64-mingw32.static-strip src/flnet.exe
 make nsisinst
 mv src/*setup*exe .
-
-make clean
-
-# build the distribution tarball
-./configure
-make distcheck
-make clean
